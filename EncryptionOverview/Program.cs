a0c0ae71a34dc09d6c58e936bc0259b8a519a423
Albert Soczewski <albert.soczewski@gmail.com>
2018-11-29 00:45:38 +0100

    Adds System.Cryptography symmetric algorithms

--- EncryptionOverview/Program.cs
+++ EncryptionOverview/Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace EncryptionOverview
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var password = "E>6Qq-ae,!(<,cxP";
+            Console.WriteLine("Application is loading... Please wait");
+            var systemCrypto = new SystemCryptography();
+
+            var filePath = GetFilePath();
+            var message = await File.ReadAllTextAsync(filePath);
+
+            var messageRaw = Encoding.UTF8.GetBytes(message);
+
+            var rijndaelEncryptedMessageRaw = systemCrypto.EncryptRijndael(messageRaw, password);
+            var rijndaelDecryptedMessageRaw = systemCrypto.DecryptRijndael(rijndaelEncryptedMessageRaw, password);
+            var rijndaelDecryptedMessage = Encoding.UTF8.GetString(rijndaelDecryptedMessageRaw);
+
+            var tripleDesEncryptedMessageRaw = systemCrypto.EncryptTripleDes(messageRaw, password);
+            var tripleDesDecryptedMessageRaw = systemCrypto.DecryptTripleDes(tripleDesEncryptedMessageRaw, password);
+            var tripleDesDecryptedMessage = Encoding.UTF8.GetString(tripleDesDecryptedMessageRaw);
+
+            Console.WriteLine($"Original message: {message}");
+            Console.WriteLine($"Rijndael decrypted message: {rijndaelDecryptedMessage}");
+            Console.WriteLine($"Triple DES decrypted message: {tripleDesDecryptedMessage}");
+
+            var areSame = AreSame(message, rijndaelDecryptedMessage, tripleDesDecryptedMessage);
+
+            Console.WriteLine($"Are same? {areSame}");
+
+            Console.ReadKey();
+        }
+
+        private static bool AreSame(string message, string rijndaelDecryptedMessage, string tripleDesDecryptedMessage)
+        {
+            return string.Equals(message, rijndaelDecryptedMessage)
+                && string.Equals(message, tripleDesDecryptedMessage);
+        }
+
+        private static string GetFilePath()
+        {
+            string filePath;
+
+            do
+            {
+                Console.WriteLine("Please give file path");
+                filePath = Console.ReadLine();
+            } while (!File.Exists(filePath));
+
+            return filePath;
         }
     }
 }
---
+++ EncryptionOverview/SystemCryptography.cs
@@ -0,0 +1,93 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+
+namespace EncryptionOverview
+{
+    public class SystemCryptography
+    {
+        private static readonly byte[] SALT =
+        {
+            0x26, 0xdc, 0xff, 0x00, 0xad, 0xed, 0x7a, 0xee, 0xc5, 0xfe, 0x07, 0xaf, 0x4d, 0x08, 0x22, 0x3c
+        };
+
+        public byte[] EncryptRijndael(byte[] plain, string password)
+        {
+            using (var rijndael = GetRijndael(password))
+            {
+                return GetCipher(plain, rijndael);
+            }
+        }
+
+        public byte[] DecryptRijndael(byte[] cipher, string password)
+        {
+            using (var rijndael = GetRijndael(password))
+            {
+                return GetResult(cipher, rijndael);
+            }
+        }
+
+        public byte[] EncryptTripleDes(byte[] plain, string password)
+        {
+            using (var tripleDes = GetTripleDes(password))
+            {
+                return GetCipher(plain, tripleDes);
+            }
+        }
+
+        public byte[] DecryptTripleDes(byte[] cipher, string password)
+        {
+            using (var tripleDes = GetTripleDes(password))
+            {
+                return GetResult(cipher, tripleDes);
+            }
+        }
+
+        private static RijndaelManaged GetRijndael(string password)
+        {
+            var rijndael = new RijndaelManaged();
+            var pdb = GetDeriveBytes(password);
+
+            rijndael.Key = pdb.GetBytes(32);
+            rijndael.IV = pdb.GetBytes(16);
+
+            return rijndael;
+        }
+
+        private static TripleDESCryptoServiceProvider GetTripleDes(string password)
+        {
+            var tripleDes = new TripleDESCryptoServiceProvider();
+            var pdb = GetDeriveBytes(password);
+
+            tripleDes.Key = pdb.GetBytes(24);
+            tripleDes.IV = pdb.GetBytes(8);
+
+            return tripleDes;
+        }
+
+        private static byte[] GetResult(byte[] cipher, SymmetricAlgorithm algorithm)
+        {
+            using (var memoryStream = new MemoryStream())
+            using (var cryptoStream = new CryptoStream(memoryStream, algorithm.CreateDecryptor(), CryptoStreamMode.Write))
+            {
+                cryptoStream.Write(cipher, 0, cipher.Length);
+                cryptoStream.Close();
+
+                return memoryStream.ToArray();
+            }
+        }
+
+        private static byte[] GetCipher(byte[] plainText, SymmetricAlgorithm algorithm)
+        {
+            using (var memoryStream = new MemoryStream())
+            using (var cryptoStream = new CryptoStream(memoryStream, algorithm.CreateEncryptor(), CryptoStreamMode.Write))
+            {
+                cryptoStream.Write(plainText, 0, plainText.Length);
+                cryptoStream.Close();
+
+                return memoryStream.ToArray();
+            }
+        }
+
+        private static Rfc2898DeriveBytes GetDeriveBytes(string password) => new Rfc2898DeriveBytes(password, SALT);
+    }
+}
